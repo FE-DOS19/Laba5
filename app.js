6eec24df433367959d67a5c12326710cc7257659
FE-DOS19 <tfedor26@gmail.com>
2025-10-08 12:18:40 +0400

    Update app.js

--- app.js
+++ app.js
@@ -1,45 +1,44 @@
 let tg = window.Telegram.WebApp;
-
 tg.expand();
-
 tg.MainButton.textColor = "#FFFFFF";
-tg.MainButton.color = "#4CAF50";
+tg.MainButton.color = "#2cab37";
 
-let selectedLevel = "";
+let level = "";
+
 let btn1 = document.getElementById("btn1");
 let btn2 = document.getElementById("btn2");
 let btn3 = document.getElementById("btn3");
 
-btn1.addEventListener("click", function() {
+btn1.addEventListener("click", function () {
     if (tg.MainButton.isVisible) {
         tg.MainButton.hide();
     } else {
         tg.MainButton.setText("Начать тест начального уровня");
-        selectedLevel = "beginner";
+        level = "beginner";
         tg.MainButton.show();
     }
 });
 
-btn2.addEventListener("click", function() {
+btn2.addEventListener("click", function () {
     if (tg.MainButton.isVisible) {
         tg.MainButton.hide();
     } else {
         tg.MainButton.setText("Начать тест среднего уровня");
-        selectedLevel = "intermediate";
+        level = "intermediate";
         tg.MainButton.show();
     }
 });
 
-btn3.addEventListener("click", function() {
+btn3.addEventListener("click", function () {
     if (tg.MainButton.isVisible) {
         tg.MainButton.hide();
     } else {
         tg.MainButton.setText("Начать тест продвинутого уровня");
-        selectedLevel = "advanced";
+        level = "advanced";
         tg.MainButton.show();
     }
 });
 
-Telegram.WebApp.onEvent("mainButtonClicked", function() {
-    tg.sendData(selectedLevel);
+Telegram.WebApp.onEvent("mainButtonClicked", function () {
+    tg.sendData(level);
 });
